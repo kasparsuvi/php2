0d5911a6fdac808ab82f7c3eae2b1e7a6566b4d4
Kaspar <kaspar.suvi@khk.ee>
2017-01-24 14:22:06 +0200

    Change object construct - allow use template name by parameter
    Ilma parameetrita ei saa enam template teha.

--- classes/template.php
+++ classes/template.php
@@ -16,6 +16,12 @@ class template{
     var $file = ''; // template file name
     var $content = false; // template content - now is empty
     // class methods
+    // construct
+    function __construct($f){
+        $this->file = $f;
+        $this->loadFile();
+    }// construct
+
     function loadFile(){
         $f = $this->file; // use file name variable
         // if some problem with tmpl directory
