7abf43bd62e45b2557433738b0fcc85ebc342d90
Kaspar <kaspar.suvi@khk.ee>
2017-01-24 15:23:58 +0200

    Create initial version of linkobject class

---
+++ classes/linkobject.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Kaspar
+ * Date: 1/24/2017
+ * Time: 3:23 PM
+ */
+// only for testing
+// import http class
+require_once 'http.php';
+// only for testing
+class linkobject extends http{
+
+}
