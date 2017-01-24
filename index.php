3765321e5c520052b7fd938414e3f7398fbcba86
Kaspar <kaspar.suvi@khk.ee>
2017-01-24 14:49:38 +0200

    Create new version of main template

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 // create template object
 define('CLASSES_DIR', 'classes/');
 define('TMPL_DIR', 'tmpl/');
+define('STYLE_DIR', 'css/');
 require_once CLASSES_DIR.'template.php';
 // and use it
 // create an template object,
@@ -16,14 +17,18 @@ require_once CLASSES_DIR.'template.php';
 // load template file content
 $tmpl = new template('main');
 // add pairs of temlate element names and real values
+$tmpl->set('style', STYLE_DIR.'main.css');
+
+$tmpl->set('header', 'minu lehe pealkiri');
 $tmpl->set('menu', 'minu menüü');
 $tmpl->set('nav_bar', 'minu navigatsioon');
 $tmpl->set('lang_bar', 'minu keeleriba');
 $tmpl->set('content', 'minu sisu');
 // control the content of template object
-echo '<pre>';
+/*echo '<pre>';
 print_r($tmpl);
 echo '</pre>';
-
+*/
 echo $tmpl->parse();
+
 ?>
