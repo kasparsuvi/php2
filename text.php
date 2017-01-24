2c1c5cc65dbe196694a83405ef0d1eb08f490ae9
Kaspar <kaspar.suvi@khk.ee>
2017-01-24 12:51:06 +0200

    text class initial version Made text class!
    With functions setText and show

---
+++ text.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Kaspar
+ * Date: 1/24/2017
+ * Time: 12:48 PM
+ */
+class text
+{// text class begin
+    // class variables = instance variables
+    var $str = '';
+    // methods
+    // set text function
+    function setText($s){
+        $this->str = $s;
+    }// setText
+    // show text function
+    function show(){
+        echo $this->str.'<br/>';
+    }// show
+}// text class end
+?>
