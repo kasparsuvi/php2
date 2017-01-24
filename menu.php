78a875bf0477ee78f60e998c20aeaf3e13f0e081
Kaspar <kaspar.suvi@khk.ee>
2017-01-24 20:57:50 +0200

    Create real menu element and control this element output

--- menu.php
+++ menu.php
@@ -10,9 +10,12 @@
 // for menu and menu itmes
 $menu = new template('menu.menu'); // in menu directory is file menu.html menu/menu.html
 $item = new template('menu.item');
-
+// add pairs of item temlate element names and real values
+$item->set('name', 'Esimene leht');
+$link = $http->getLink('page'=>'first');
+$item->set('link', $link);
+// control created item output
 echo '<pre>';
-print_r($menu);
 print_r($item);
 echo '</pre>';
 ?>
