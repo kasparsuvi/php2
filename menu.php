69332205497c1e80ad668c09a6dce7dfd2dd597e
Kaspar <kaspar.suvi@khk.ee>
2017-01-24 21:01:16 +0200

    Output menu object real value

--- menu.php
+++ menu.php
@@ -15,7 +15,15 @@ $item->set('name', 'Esimene leht');
 $link = $http->getLink(array('page'=>'first'));
 $item->set('link', $link);
 // control created item output
+/*echo '<pre>';
+  print_r($item);
+  echo '</pre>';*/
+// add menu item to menu
+$menu->set('items', $item->parse());
+// control created menu output
 echo '<pre>';
-print_r($item);
-echo '</pre>';
+ print_r($menu);
+  echo '</pre>';
+ // output menu
+ echo $menu->parse();
 ?>
