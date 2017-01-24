692650ae4cb3e8ab3b7fa36e49f0392a2cace8a4
Kaspar <kaspar.suvi@khk.ee>
2017-01-24 12:55:24 +0200

    Test fot text class(es)
    Proov text.php failis loodud text classi funktsioonide testimine.
    
    -setText()
    -show()

--- test.php
+++ test.php
@@ -6,6 +6,21 @@
  * Time: 12:34 PM
  */
 
-echo 'php stormiga koostatud test ';
+// require the object creating and using class
+require_once('text.php');
+// create an object
+$sentence = new text();
+// control object output
+echo '<pre>';
+print_r($sentence);
+echo '</pre>';
+//set text
+$sentence->setText("Hello text object!");
+// control object output
+echo '<pre>';
+print_r($sentence);
+echo '</pre>';
+// show object output
+$sentence->show();
 
 ?>
