43e9716888a4ca66ab710cece468b896f27fe382
Kaspar <kaspar.suvi@khk.ee>
2017-01-24 14:54:22 +0200

    Define style directory constand and use main page style sheet

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require_once CLASSES_DIR.'template.php';
 // load template file content
 $tmpl = new template('main');
 // add pairs of temlate element names and real values
-$tmpl->set('style', STYLE_DIR.'main.css');
+$tmpl->set('style', STYLE_DIR.'main'.'.css');
 
 $tmpl->set('header', 'minu lehe pealkiri');
 $tmpl->set('menu', 'minu menüü');
