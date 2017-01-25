d6551e2efa544a9c217d6ec0af430db5c616b261
Kaspar <kaspar.suvi@khk.ee>
2017-01-25 18:17:41 +0200

    Remove configuration from index

---
+++ conf.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaspar
+ * Date: 1/25/2017
+ * Time: 6:15 PM
+ */
+// create and template object
+define('CLASSES_DIR', 'classes/'); // classes path
+define('TMPL_DIR', 'tmpl/'); // templates path
+define('STYLE_DIR', 'css/'); // styles path
+define('ACTS_DIR', 'acts/'); // acts path
+define('DEFAULT_ACT', 'default'); // default act file name
+
+require_once CLASSES_DIR.'template.php'; // import template class
+require_once CLASSES_DIR.'http.php'; // import http class
+require_once CLASSES_DIR.'linkobject.php'; // import linkobject class
+
+
+// create and output http object from linkobject class
+$http = new linkobject();
+?>
--- index.php
+++ index.php
@@ -6,14 +6,6 @@
  * Date: 12.01.2017
  * Time: 12:58
  */
-// create and template object
-define('CLASSES_DIR', 'classes/'); // classes path
-define('TMPL_DIR', 'tmpl/'); // templates path
-define('STYLE_DIR', 'css/'); // styles path
-define('ACTS_DIR', 'acts/'); // acts path
-define('DEFAULT_ACT', 'default'); // default act file name
-require_once CLASSES_DIR.'template.php';
-// and use it
 // create an template object,
 // set up the file name for template
 // load template file content
@@ -21,16 +13,12 @@ $tmpl = new template('main');
 // add pairs of temlate element names and real values
 $tmpl->set('style', STYLE_DIR.'main'.'.css');
 $tmpl->set('header', 'minu lehe pealkiri');
-// import http class
-require_once CLASSES_DIR.'http.php';
-// import linkobject class
-require_once CLASSES_DIR.'linkobject.php';
-// create and output http object from linkobject class
-$http = new linkobject();
+
 // create and output menu
 // import menu file
 require_once 'menu.php'; // in this file is menu creation
 $tmpl->set('menu', $menu->parse());
+
 $tmpl->set('nav_bar', 'minu navigatsioon');
 $tmpl->set('lang_bar', 'minu keeleriba');
 $tmpl->set('content', 'minu sisu');
