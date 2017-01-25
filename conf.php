040cf53881538a689aa4e5a733221418f7286498
Kaspar <kaspar.suvi@khk.ee>
2017-01-25 18:23:30 +0200

    Import database class and create database object

--- conf.php
+++ conf.php
@@ -16,8 +16,11 @@ define('DEFAULT_ACT', 'default'); // default act file name
 require_once CLASSES_DIR.'template.php'; // import template class
 require_once CLASSES_DIR.'http.php'; // import http class
 require_once CLASSES_DIR.'linkobject.php'; // import linkobject class
+require_once CLASSES_DIR.'mysql.php'; // import database class
 
 
 // create and output http object from linkobject class
 $http = new linkobject();
+// create database object
+$db = new mysql('localhost', 'kasparsuvi', '79nF49QZ', 'kasparsuvi_arst');
 ?>
