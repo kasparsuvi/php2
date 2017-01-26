170af1107dc4e6d5db91f2a4ee0675a4e2d4e316
Kaspar <kaspar.suvi@khk.ee>
2017-01-26 14:56:38 +0200

    Add utils.php to conf file

--- conf.php
+++ conf.php
@@ -12,6 +12,10 @@ define('TMPL_DIR', 'tmpl/'); // templates path
 define('STYLE_DIR', 'css/'); // styles path
 define('ACTS_DIR', 'acts/'); // acts path
 define('DEFAULT_ACT', 'default'); // default act file name
+define('LIB_DIR'),'lib/');// lib path
+
+//import useful functions
+require_once LIB_DIR.'utils.php';
 
 require_once CLASSES_DIR.'template.php'; // import template class
 require_once CLASSES_DIR.'http.php'; // import http class
