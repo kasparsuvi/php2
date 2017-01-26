fbe0df1a89607ba0580fb1665056919db3f6dc91
Kaspar <kaspar.suvi@khk.ee>
2017-01-26 15:18:51 +0200

    create session

--- classes/session.php
+++ classes/session.php
@@ -24,6 +24,35 @@ class session{ //session start
 
     }//construct
 
+    //create session
+    function createSession($user = false){
+        //anonymous session
+        if ($user == false){
+            $user = array(
+                'user_id' => 0,
+                'role_id' => 0,
+                'username' => 'Anonymous'
+            );
+        }
+        // create session id
+        $sid =md5(uniqid(time().mt_rand(1,1000), true));
+        //insert data to database
+        $sql = 'INSERT INTO session SET '.
+            'sid='.fixDB($sid).', '.
+            'user_id='.fixDB($user['user_id']).', '.
+            'user_data='.fixDB(serialize($user)).','.
+            'login_ip='.fixDB(REMOTE_ADDR).', '.
+            'created=NOW()';
+
+        $this->db->query($sql);
+
+        // setup session id number
+        $this->sid = $sid;
+        $this->http->set('sid',$sid);
+
+
+    }createSession
+
 }// session end
 
 
