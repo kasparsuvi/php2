f9750d772647bf4cca87be712685699c24cd1228
Kaspar <kaspar.suvi@khk.ee>
2017-02-06 10:54:37 +0200

    added default language=et

--- conf.php
+++ conf.php
@@ -13,24 +13,34 @@ define('STYLE_DIR', 'css/'); // styles path
 define('ACTS_DIR', 'acts/'); // acts path
 define('LIB_DIR', 'lib/'); // lib path
 define('DEFAULT_ACT', 'default'); // default act file name
+
 // user roles
 define('ROLE_NONE', 0);
 define('ROLE_ADMIN', 1);
 define('ROLE_USER', 2);
+
+//define default language
+define('DEFAULT_LANG','et');
+
 // import useful functions
 require_once LIB_DIR.'utils.php';
+
 require_once CLASSES_DIR.'template.php'; // import template class
 require_once CLASSES_DIR.'http.php'; // import http class
 require_once CLASSES_DIR.'linkobject.php'; // import linkobject class
 require_once CLASSES_DIR.'mysql.php'; // import database class
 require_once CLASSES_DIR.'session.php'; // import session class
 require_once 'db_conf.php'; // import database configuration
+
 // create and output http object from linkobject class
 $http = new linkobject();
+
 // create database object
 $db = new mysql(DBHOST,DBUSER,DBPASS,DBNAME);
+
 // create session object
 $sess = new session($http, $db);
 $tmpl = new template('main');
+
 require_once ACTS_DIR.'login.php'; // import login act
 ?>
