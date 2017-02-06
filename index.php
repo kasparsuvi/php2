16c6d5963bfe548150a37561ee19a9110d9e50e2
Kaspar <kaspar.suvi@khk.ee>
2017-02-06 11:23:01 +0200

    Language support work also in lang_bar

--- index.php
+++ index.php
@@ -8,24 +8,30 @@
  */
 // import configuration
 require_once 'conf.php';
-// create an template object,
-// set up the file name for template
-// load template file content
-// add pairs of temlate element names and real values
+
+//require language control
+require_once ('lang.php');
+
+// add pairs of template element names and real values
 $tmpl->set('style', STYLE_DIR.'main'.'.css');
 $tmpl->set('header', 'minu lehe pealkiri');
+
 // create and output menu
 // import menu file
 require_once 'menu.php'; // in this file is menu creation
 $tmpl->set('menu', $menu->parse());
+
+// import act file
+require_once 'act.php';
+
 $tmpl->set('nav_bar', $sess->user_data['username']);
-$tmpl->set('lang_bar', LANG_ID);
+
+//$tmpl->set('lang_bar', LANG_ID);
 $tmpl->set('content', $form->parse());
 // output template content set up with real values
 echo $tmpl->parse();
 // control actions
-// import act file
-require_once 'act.php';
+
 // control database object
 // create test query
 $sess ->flush();
