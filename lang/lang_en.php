aedf69d62c8c18d069b34057aaf2b3b3990b3870
Kaspar <kaspar.suvi@khk.ee>
2017-02-06 11:25:04 +0200

    added english translation

---
+++ lang/lang_en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaspar
+ * Date: 2/6/2017
+ * Time: 11:24 AM
+ */
+
+
+$_trans = array(
+    'esimene' => 'First',
+    'teine' => 'Second',
+    'kolmas' => 'Third',
+    'minu lehe pealkiri' => 'My page heading',
+    'logi välja' => 'Logout',
+    'kasutajanimi' => 'Username',
+    'karool' => 'Password',
+    'logi sisse' => 'Login',
+    'viga sisselogimisel' => 'Login error'
+);
+?>
